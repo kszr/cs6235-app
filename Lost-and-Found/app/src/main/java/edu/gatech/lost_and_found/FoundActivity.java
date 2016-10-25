b1aea2b0790c3da361052fa196156be075b352a8
kszr <abhishek.rob@gmail.com>
2016-10-25 01:55:16 -0400

    Adding location info

--- Lost-and-Found/app/src/main/java/edu/gatech/lost_and_found/FoundActivity.java
+++ Lost-and-Found/app/src/main/java/edu/gatech/lost_and_found/FoundActivity.java
@@ -1,10 +1,15 @@
 package edu.gatech.lost_and_found;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationManager;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +18,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 /**
  * Created by abhishekchatterjee on 10/23/16.
@@ -23,6 +30,10 @@ public class FoundActivity extends AppCompatActivity {
     private static final int CAMERA_ACTIVITY = 0;
     private static final int FOUND_AND_TURN_IN = 1;
     private static final int FOUND_AND_LEAVE = 2;
+
+    private Bitmap photo = null;
+    private Double lat = null;
+    private Double lon = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +72,7 @@ public class FoundActivity extends AppCompatActivity {
     }
 
     private void startCameraActivity() {
-        if(!(ActivityCompat.checkSelfPermission(this, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED)) {
+        if (!(ActivityCompat.checkSelfPermission(this, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED)) {
             ActivityCompat.requestPermissions(
                     this,
                     new String[]{CAMERA_PERMISSION},
@@ -79,7 +90,7 @@ public class FoundActivity extends AppCompatActivity {
      * continue, retake the image, or cancel out accordingly.
      */
     private void promptImageOkay() {
-        if(true) {
+        if (true) {
 
         } else {
 
@@ -90,32 +101,51 @@ public class FoundActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == CAMERA_ACTIVITY && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-
-            ImageButton cameraButton = (ImageButton) findViewById(R.id.button_camera);
-            assert cameraButton != null;
-            cameraButton.setClickable(false);
-
-            TextView step2 = (TextView) findViewById(R.id.step2);
-            assert step2 != null;
-            step2.setVisibility(View.VISIBLE);
-
-            TextView step2_text = (TextView) findViewById(R.id.step2_text);
-            assert step2_text != null;
-            step2_text.setVisibility(View.VISIBLE);
-
-            Button lost_and_found = (Button) findViewById(R.id.turn_in_object_button);
-            assert lost_and_found != null;
-            lost_and_found.setVisibility(View.VISIBLE);
-
-            Button keep_object = (Button) findViewById(R.id.leave_object_button);
-            assert keep_object != null;
-            keep_object.setVisibility(View.VISIBLE);
+        if (requestCode == CAMERA_ACTIVITY && resultCode == Activity.RESULT_OK) {
+            getImageData(data);
+            setButtonsStep2();
         } else if(requestCode == FOUND_AND_TURN_IN && resultCode == Activity.RESULT_OK) {
 
         } else if(requestCode == FOUND_AND_LEAVE && resultCode == Activity.RESULT_OK) {
 
         }
     }
+
+    private void getImageData(Intent data) {
+        photo = (Bitmap) data.getExtras().get("data");
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+
+        Log.d(TAG,"Captured image.");
+        Log.d(TAG,"Latitude: " + lat.toString() + "; Longitude: " + lon.toString());
+    }
+
+    private void setButtonsStep2() {
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.button_camera);
+        assert cameraButton != null;
+        cameraButton.setClickable(false);
+
+        TextView step2 = (TextView) findViewById(R.id.step2);
+        assert step2 != null;
+        step2.setVisibility(View.VISIBLE);
+
+        TextView step2_text = (TextView) findViewById(R.id.step2_text);
+        assert step2_text != null;
+        step2_text.setVisibility(View.VISIBLE);
+
+        Button lost_and_found = (Button) findViewById(R.id.turn_in_object_button);
+        assert lost_and_found != null;
+        lost_and_found.setVisibility(View.VISIBLE);
+
+        Button keep_object = (Button) findViewById(R.id.leave_object_button);
+        assert keep_object != null;
+        keep_object.setVisibility(View.VISIBLE);
+    }
+
 }
