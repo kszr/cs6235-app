92229f549269f55bb9576b560a04383542506568
kszr <abhishek.rob@gmail.com>
2016-11-17 11:19:21 -0500

    Adding a blank table thing

--- LostAndFound/app/src/main/java/edu/gatech/lostandfound/database/DBContract.java
+++ LostAndFound/app/src/main/java/edu/gatech/lostandfound/database/DBContract.java
@@ -10,11 +10,14 @@ public class DBContract {
     // make the constructor private.
     private DBContract() {}
 
-    /* Inner class that defines the table contents */
     public static class DBEntry implements BaseColumns {
         public static final String TABLE_NAME = "entry";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+
+    public static class PotentialLostAndFound implements BaseColumns {
+        
     }
 }
 
